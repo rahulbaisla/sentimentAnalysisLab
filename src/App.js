25ca4d39051341a4752acaf58d008994d752fbd5
rahul <r@gmail.com>
2020-03-05 00:54:13 +1100

    modified and formatted App.js

--- src/App.js
+++ src/App.js
@@ -1,119 +1,92 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import "./App.css";
 
-import './App.css';
-import Amplify, { Predictions } from 'aws-amplify';
-import { AmazonAIPredictionsProvider } from '@aws-amplify/predictions';
-import mic from 'microphone-stream';
-import { withAuthenticator, AmplifyTheme} from 'aws-amplify-react';
-import GaugeChart from 'react-gauge-chart'
-import JSONPretty from 'react-json-pretty';
-import awsconfig from './aws-exports';
-import { makeStyles } from '@material-ui/core/styles';
-import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
-import orange from '@material-ui/core/colors/orange'
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
+// Amplify 
+import Amplify, { Predictions } from "aws-amplify";
+import { AmazonAIPredictionsProvider } from "@aws-amplify/predictions";
+import { withAuthenticator, AmplifyTheme } from "aws-amplify-react";
+import awsconfig from "./aws-exports";
+
+// Utilities
+import mic from "microphone-stream";
+import GaugeChart from "react-gauge-chart";
+import JSONPretty from "react-json-pretty";
+
+// material-ui
+import { makeStyles } from "@material-ui/core/styles";
+import { createMuiTheme, MuiThemeProvider } from "@material-ui/core/styles";
+import orange from "@material-ui/core/colors/orange";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
 
 const theme = createMuiTheme({
-    palette: {
-      primary: orange,
-    }
-  });
+  palette: {
+    primary: orange
+  }
+});
 
-  
 const useStyles = makeStyles(theme => ({
-    root: {
-      flexGrow: 1,
-      height: "100vh"
-    },
-    container: {
-        fontFamily: "-apple-system,\n                BlinkMacSystemFont,\n                \"Segoe UI\",\n                Roboto,\n                \"Helvetica Neue\",\n                Arial,\n                sans-serif,\n                \"Apple Color Emoji\",\n                \"Segoe UI Emoji\",\n                \"Segoe UI Symbol\"",
-        fontWeight: '400',
-        lineHeight: '1.5',
-        color: '#212529',
-        textAlign: 'center',
-        paddingLeft: '15px',
-        paddingRight: '15px',
-        height: '100%'
-    },
-    recBtnContainer:{
-        display: "flex",
-        justifyContent: "space-evenly",
-        padding: '30px 10px 30px 10px'
-    },
-    gaugeContainer: {
-        display: "flex",
-        justifyContent: "space-evenly",
-        padding: '30px 10px 30px 10px',
-        height: '100%',
-    },
-    textContainer : {
-        display: "flex",
-        padding: '5px 5px 5px 5px',
-        border: '3px solid transparent',
-        borderColor: '#ccc',
-        width: '70%',
-        height: '65%',
-        textAlign: 'left'
-    },
-    button: {
-    display: 'inline-block',
-    padding: '6px 12px',
-    marginBottom: '0',
-    fontSize: '14px',
-    fontWeight: 400,
-    lineHeight: '1.42857143',
-    textAlign: 'center',
-    whiteSpace: 'nowrap',
-    verticalAlign: 'middle',
-    touchAction: 'manipulation',
-    cursor: 'pointer',
-    userSelect: 'none',
-    backgroundImage: 'none',
-    border: '1px solid transparent',
-    borderRadius: '4px',
-    color: '#333',
-    backgroundColor: '#fff',
-    borderColor: '#ccc',
-    },
-    menuButton: {
-      marginRight: theme.spacing(2),
-    },
-    title: {
-      flexGrow: 1,
-    },
-    list: {
-      width: 250,
-    },
-    fullList: {
-      width: 'auto',
-    },
-    chartStyle : {
-        height: 250
-    },
-    chartDivStyle : {
-        height: 300, 
-        width: 540,
-        border: 3
-      }
-  }));
+  root: {
+    flexGrow: 1,
+    height: "100vh"
+  },
+  container: {
+    fontFamily:
+      '-apple-system,\n                BlinkMacSystemFont,\n                "Segoe UI",\n                Roboto,\n                "Helvetica Neue",\n                Arial,\n                sans-serif,\n                "Apple Color Emoji",\n                "Segoe UI Emoji",\n                "Segoe UI Symbol"',
+    fontWeight: "400",
+    lineHeight: "1.5",
+    color: "#212529",
+    textAlign: "center",
+    paddingLeft: "15px",
+    paddingRight: "15px",
+    height: "100%"
+  },
+  recBtnContainer: {
+    display: "flex",
+    justifyContent: "space-evenly",
+    padding: "30px 10px 30px 10px"
+  },
+  gaugeContainer: {
+    display: "flex",
+    justifyContent: "space-evenly",
+    padding: "30px 10px 30px 10px",
+    height: "100%"
+  },
+  textContainer: {
+    display: "flex",
+    padding: "5px 5px 5px 5px",
+    border: "3px solid transparent",
+    borderColor: "#ccc",
+    width: "70%",
+    height: "65%",
+    textAlign: "left"
+  },
+  chartStyle: {
+    height: 250
+  },
+  chartDivStyle: {
+    height: 300,
+    width: 540,
+    border: 3
+  }
+}));
 
 Amplify.configure(awsconfig);
 
 Amplify.addPluggable(new AmazonAIPredictionsProvider());
 
-
+/** Function to convert speech to text  */
 function SpeechToText(props) {
   const classes = props.classes;
-  console.log("SpeechToText :: props :: " + props)
-  console.log("SpeechToText :: classes :: " + classes)
-  
-  const [response, setResponse] = useState("")
+  console.log("SpeechToText :: props :: " + props);
+  console.log("SpeechToText :: classes :: " + classes);
+
+  const [response, setResponse] = useState("");
 
   const [percent, setPercent] = useState(0);
-  
-  function AudioRecorder(props) {
 
+  /** Function to Start/Stop Audio Recording  */
+  function AudioRecorder(props) {
     const [recording, setRecording] = useState(false);
     const [micStream, setMicStream] = useState();
     const [audioBuffer] = useState(
@@ -127,7 +100,7 @@ function SpeechToText(props) {
           console.log("reseting buffer");
           buffer = [];
         }
- 
+
         return {
           reset: function() {
             newBuffer();
@@ -143,30 +116,30 @@ function SpeechToText(props) {
     );
 
     async function startRecording() {
-      console.log('start recording');
+      console.log("start recording");
       audioBuffer.reset();
 
-      window.navigator.mediaDevices.getUserMedia({ video: false, audio: true }).then((stream) => {
-        const startMic = new mic();
+      window.navigator.mediaDevices
+        .getUserMedia({ video: false, audio: true })
+        .then(stream => {
+          const startMic = new mic();
 
-        startMic.setStream(stream);
-        startMic.on('data', (chunk) => {
-          var raw = mic.toRaw(chunk);
-          if (raw == null) {
-            return;
-          }
-          audioBuffer.addData(raw);
+          startMic.setStream(stream);
+          startMic.on("data", chunk => {
+            var raw = mic.toRaw(chunk);
+            if (raw == null) {
+              return;
+            }
+            audioBuffer.addData(raw);
+          });
 
+          setRecording(true);
+          setMicStream(startMic);
         });
-
-        setRecording(true);
-        setMicStream(startMic);
-        
-      });
     }
 
     async function stopRecording() {
-      console.log('stop recording');
+      console.log("stop recording");
       const { finishRecording } = props;
 
       micStream.stop();
@@ -178,102 +151,133 @@ function SpeechToText(props) {
       if (typeof finishRecording === "function") {
         finishRecording(resultBuffer);
       }
-
     }
 
     return (
       <div>
         <div className={classes.recBtnContainer}>
-        <MuiThemeProvider theme={theme}>
-        <Button id = "stopRecBtn" disabled={!recording} variant="contained" color="primary" onClick={stopRecording} hidden={true}>Stop recording</Button>
-        <Button  id = "startRecBtn" disabled={recording} variant="contained" color="primary" onClick={startRecording} hidden={true}>Start recording</Button>
-        </MuiThemeProvider>
-        
-          
+          <MuiThemeProvider theme={theme}>
+            <Button
+              id="stopRecBtn"
+              disabled={!recording}
+              variant="contained"
+              color="primary"
+              onClick={stopRecording}
+              hidden={true}
+            >
+              Stop recording
+            </Button>
+            <Button
+              id="startRecBtn"
+              disabled={recording}
+              variant="contained"
+              color="primary"
+              onClick={startRecording}
+              hidden={true}
+            >
+              Start recording
+            </Button>
+          </MuiThemeProvider>
         </div>
       </div>
     );
   }
 
+  /** Function to convert recorded audio to text using Amazon Transcribe   */
   function convertFromBuffer(bytes) {
-    setResponse('Performing Sentiment Analysis...');
+    setResponse("Performing Sentiment Analysis...");
 
     Predictions.convert({
-        transcription: {
-          source: {
-            bytes
-          },
-           language: "en-US", // other options are "en-GB", "fr-FR", "fr-CA", "es-US"
+      transcription: {
+        source: {
+          bytes
         },
-      }).then(({ transcription: { fullText } }) => {console.log(fullText);interpretFromPredictions(JSON.stringify(fullText, null, 2))})
-        .catch(err => console.log(JSON.stringify(err, null, 2)))
-
-        
+        language: "en-US" // other options are "en-GB", "fr-FR", "fr-CA", "es-US"
+      }
+    })
+      .then(({ transcription: { fullText } }) => {
+        console.log(fullText);
+        interpretFromPredictions(JSON.stringify(fullText, null, 2));
+      })
+      .catch(err => console.log(JSON.stringify(err, null, 2)));
   }
 
+  /** Function to apply sentiment analysis on converted text using Amazon Comprehend */
   function interpretFromPredictions(textToInterpret) {
-    console.log("inside interpretFromPredictions")
+    console.log("inside interpretFromPredictions");
     Predictions.interpret({
       text: {
         source: {
-          text: textToInterpret,
+          text: textToInterpret
         },
         type: "ALL"
       }
-    }).then(result => {setResponse(JSON.stringify(result, null, 2));setGauge(result);})
-      .catch(err => setResponse(JSON.stringify(err, null, 2)))
+    })
+      .then(result => {
+        var textToDisplay = textToInterpret + "\n\n" + JSON.stringify(result, null, 2);
+        setResponse(textToDisplay);
+        setGauge(result);
+      })
+      .catch(err => setResponse(JSON.stringify(err, null, 2)));
   }
 
   function setGauge(result) {
-    
-    if (result.textInterpretation.sentiment.predominant === 'POSITIVE') {
-        console.log("Inside condition POSITIVE :: ")
-        //setPercent(JSON.stringify(result.textInterpretation.sentiment.positive))
-        setPercent(0.83)
-    } else if (result.textInterpretation.sentiment.predominant === 'NEGATIVE') {
-        console.log("Inside condition NEGATIVE :: ")
-        //setPercent(JSON.stringify(result.textInterpretation.sentiment.negative))
-        setPercent(0.17)
-    } else if (result.textInterpretation.sentiment.predominant === 'NEUTRAL') {
-        console.log("Inside condition NEUTRAL :: ")
-        //setPercent(JSON.stringify(result.textInterpretation.sentiment.negative))
-        setPercent(0.50)
+    if (result.textInterpretation.sentiment.predominant === "POSITIVE") {
+      console.log("Inside condition POSITIVE :: ");
+      setPercent(0.83);
+    } else if (result.textInterpretation.sentiment.predominant === "NEGATIVE") {
+      console.log("Inside condition NEGATIVE :: ");
+      setPercent(0.17);
+    } else if (result.textInterpretation.sentiment.predominant === "NEUTRAL") {
+      console.log("Inside condition NEUTRAL :: ");
+      setPercent(0.5);
     }
-    
-
   }
   return (
-      <div className={classes.container}>
-        <AudioRecorder classes = {classes} finishRecording={convertFromBuffer} />
-        <div className={classes.gaugeContainer}>
-            <div className={classes.chartDivStyle}>
-                <GaugeChart id="gauge-chart1"  className={classes.chartStyle} percent={percent} hideText={true}
-                colors={['#FF0000', '#FFFF00', '#00FF00']}/>
-            </div>
-            <div className={classes.textContainer}>
-            <Typography display="initial" noWrap={true} align="left" variant="h6" gutterBottom>
-                <JSONPretty id="json-pretty" data={response}></JSONPretty>
-            </Typography>
-                
-            </div>
-            
+    <div className={classes.container}>
+      <AudioRecorder classes={classes} finishRecording={convertFromBuffer} />
+      <div className={classes.gaugeContainer}>
+        <div className={classes.chartDivStyle}>
+          <GaugeChart
+            id="gauge-chart1"
+            className={classes.chartStyle}
+            percent={percent}
+            hideText={true}
+            colors={["#FF0000", "#FFFF00", "#00FF00"]}
+          />
+        </div>
+        <div className={classes.textContainer}>
+          <Typography
+            display="initial"
+            noWrap={true}
+            align="left"
+            variant="h6"
+            gutterBottom
+          >
+            <JSONPretty id="json-pretty" data={response}></JSONPretty>
+          </Typography>
         </div>
       </div>
+    </div>
   );
 }
-
 
 function App() {
   const classes = useStyles();
 
   return (
     <div className={classes.root}>
-        <div className={classes.container}>
-            <h1>Voice Sentiment Analysis</h1>
-            <SpeechToText classes={classes}/>
-        </div>
+      <div className={classes.container}>
+        <h1>Voice Sentiment Analysis</h1>
+        <SpeechToText classes={classes} />
+      </div>
     </div>
   );
 }
 
-export default withAuthenticator(App, true);
+export default withAuthenticator(App, {
+  includeGreetings: true,
+  signUpConfig: {
+    hiddenDefaults: ["phone_number"]
+  }
+});
